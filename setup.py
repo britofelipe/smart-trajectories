e4d0d9599deb85afdd95e1e5e6effbd0d8b0b19f
Felipe Brito <arrudabritofelipe@gmail.com>
2024-05-24 09:18:55 -0300

    Corrects smart-tracjectories version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smart-trajectories',
-    version='0.1.3',
+    version='0.1.4',
     author='Felipe Brito',
     author_email='arrudabritofelipe@gmail.com',
     description='Smart Trajectories for Python',
