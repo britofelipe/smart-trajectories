2d30e3005feffc59d1d66e4d315a1071ec14c882
Felipe Brito <arrudabritofelipe@gmail.com>
2024-05-24 09:05:24 -0300

    Corrects version number to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smart-trajectories',
-    version='0.1.1',
+    version='0.1.2',
     author='Felipe Brito',
     author_email='arrudabritofelipe@gmail.com',
     description='Smart Trajectories for Python',
